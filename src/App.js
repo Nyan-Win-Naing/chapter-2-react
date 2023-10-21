1d59ded11987ae7272ebc115fd92493261df1854
Nyan-Win-Naing <nyanwinnaing1922002@gmail.com>
2023-10-21 17:32:36 +0630

    usestate-hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 import QAImage from './assets/image.jpg';
 
 function App() {
-  let name = "Hlaing Min Than";
+  // let name = "Hlaing Min Than";
+
+  let [name, setName] = useState("Hlaing Min Than");
+
+  let changeName = () => {
+    // name = "Aung Aung";
+    setName("Aung Aung");
+    console.log(name);
+  }
+
   return (
     <div className='app'>
       <h1>Hello {name}</h1>
-
+      <button onClick={changeName}>change name</button>
       {/* 1st way - public folder way */}
       {/* absolute path */}
       {/* <img src="http://localhost:3000/image.jpg" alt="" /> */}
